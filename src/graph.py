d32da3f4a1395d11a0cb6a3dcdc6b1d2d2254c65
Fazle Mawla Wahyuhanda <91876138+elzaff@users.noreply.github.com>
2025-03-17 22:42:16 +0700

    Update graph.py

--- src/graph.py
+++ src/graph.py
@@ -1,22 +1,18 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 
-# 1.. create_graph
-def create_graph(edges: list[tuple[int, int]]) -> nx.Graph:
+# 1. create_graph
+def create_graph(edges:list[tuple[int, int]]) -> nx.Graph:
     G = nx.Graph()
     G.add_edges_from(edges)
     return G
 
 #2. get_degree
 def get_degree(G: nx.Graph, node: int) -> int:
-    if node not in G:
-        raise ValueError(f"Node {node} tidak ditemukan dalam graf.")
     return G.degree[node]
 
 #3. dfs_traversal
 def dfs_traversal(G: nx.Graph, start: int) -> list[int]:
-    if start not in G:
-        raise ValueError(f"Node {start} tidak ditemukan dalam graf.")
     return list(nx.dfs_preorder_nodes(G, start))
 
 #4. bfs_traversal
@@ -29,8 +25,5 @@ def find_shortest_path(G: nx.Graph, source: int, target: int) -> list[int]:
 
 #6. visualize_graph
 def visualize_graph(G: nx.Graph) -> None:
-    plt.figure(figsize=(8, 6))
-    pos = nx.spring_layout(G)
-    nx.draw(G, pos, with_labels=True, node_color='lightblue', edge_color='gray', node_size=2000, font_size=12)
-    plt.savefig("graph_visualization.png")
+    nx.draw(G, with_labels=True)
     plt.show()
